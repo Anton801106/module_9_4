e7c3769ba076b5c62702beec59d69e5c123e3198
Anton801106 <listboxx@yandex.ru>
2024-10-06 19:21:17 +0300

    Add files via upload

---
+++ module_9_4.py
@@ -0,0 +1,52 @@
+# Домашнее задание по теме "Создание функций на лету"
+# Задача "Функциональное разнообразие"
+# Даны 2 строки. Необходимо составить lambda-функцию для следующего выражения - list(map(?, first, second)).
+
+first = 'Мама мыла раму'
+second = 'Рамена мало было'
+
+print(list(map(lambda x, y: x == y, first, second)))
+
+
+# Напишите функцию get_advanced_writer(file_name), принимающую название файла для записи.
+# Внутри этой функции, напишите ещё одну - write_everything(*data_set), где *data_set - параметр принимающий неограниченное количество данных любого типа.
+# Логика write_everything заключается в добавлении в файл file_name всех данных из data_set в том же виде.
+# Функция get_advanced_writer возвращает функцию write_everything.
+def get_advanced_writer(file_name):
+    def write_everything(*data_set):
+        with open(file_name, 'a', encoding='utf-8') as file:
+            for i in data_set:
+                file.write(f'{i}\n')
+
+    return write_everything
+
+
+write = get_advanced_writer('example.txt')
+write('Это строчка проверки', ['А', 'это', 'уже', 'число', 654, 'в', 'списке', 'для', 'проверки', 'решения'])
+
+write = get_advanced_writer('xmpl.txt')
+write('Это строчка', ['А', 'это', 'уже', 'число', 100, 'в', 'списке', 'для', 'проверки', 'решения'])
+
+# Метод __call__:
+# Создайте класс MysticBall, объекты которого обладают атрибутом words хранящий коллекцию строк.
+# В этом классе также определите метод __call__ который будет случайным образом выбирать слово из words
+# и возвращать его. Для случайного выбора с одинаковой вероятностью для каждого данного
+# в коллекции можете использовать функцию choice из модуля random.
+
+from random import choice
+
+
+class MysticBall:
+    def __init__(self, *words):
+        self.words = words
+
+    def __call__(self, *words):
+        return choice(self.words)
+
+
+first_ball = MysticBall('Да', 'Нет', 'Наверное', 'Может быть')
+second_ball = MysticBall('Всегда', 'Иногда', 'Часто', 'Редко', 'Никогда')
+print(first_ball())
+print(first_ball())
+print(second_ball())
+print(second_ball())
